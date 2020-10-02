3f1f65fa5eb683c3d804cd8841ca32c1074bc4d2
Darhyppo <darhyppo@gmail.com>
2020-10-02 19:57:03 -0400

    test hero db

--- src/main/java/com/project/darphe/mywebapp/MyApplication.java
+++ src/main/java/com/project/darphe/mywebapp/MyApplication.java
@@ -1,7 +1,12 @@
 package com.project.darphe.mywebapp;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyApplication {
@@ -9,5 +14,21 @@ public class MyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
 }
