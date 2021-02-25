0cb71f24a70de0964b605326c578a2661b5777f2
Darhyppo <darhyppo@gmail.com>
2021-02-25 16:17:16 -0500

    Updatedd...

--- src/test/java/com/project/darphe/mywebappv1/MywebappV1ApplicationTests.java
+++ src/test/java/com/project/darphe/mywebappv1/MywebappV1ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.project.darphe.mywebappv1;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MywebappV1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.project.darphe.mywebappv1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MywebappV1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
